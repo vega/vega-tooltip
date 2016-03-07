5754eed8dfdf68a9523bd676be4f93f39be5638f
ZeningQu <quzening@gmail.com>
2016-03-07 12:18:04 -0800

    Use JS instead of TS. Fix #12
    Because I don't know how to use TypeScript modules!?!?

--- src/vg-tooltip.js
+++ src/vg-tooltip.js
@@ -1,6 +1,6 @@
-module vgTooltip {
 
-  export function linkToView(view) {
+
+  function linkToView(view, vgSpec) {
     // fill tooltip with data
     view.on("mouseover", fill);
 
@@ -21,24 +21,26 @@ module vgTooltip {
     // avoid showing tooltip for axis title and labels
     if (!item.datum._id) return;
 
-    let vgData = d3.map(item.datum);
+    var vgData = d3.map(item.datum);
 
     // remove vega internals
     vgData.remove("_id");
     vgData.remove("_prev");
 
     // get array of key value pairs for tooltip
-    let tooltipData = vgData.entries();
+    var tooltipData = vgData.entries();
 
     // TODO(zening): show partial data in tooltip
-    let tooltipRows = d3.select("#vis-tooltip").selectAll(".tooltip-row").data(tooltipData);
+    var tooltipRows = d3.select("#vis-tooltip").selectAll(".tooltip-row").data(tooltipData);
 
     tooltipRows.exit().remove();
 
-    let row = tooltipRows.enter().append("tr")
+    var row = tooltipRows.enter().append("tr")
     .attr("class", "tooltip-row");
     row.append("td").attr("class", "key").text(function(d) { return d.key + ":"; });
-    row.append("td").attr("class", "value").text(function(d) { return d.value.toString(); });
+    row.append("td").attr("class", "value").text(function(d) {
+      return d.value.toString();
+    });
 
     update_position(event);
     d3.select("#vis-tooltip").style("opacity", 1);
@@ -53,7 +55,7 @@ module vgTooltip {
     .style("top", function() {
       // by default: put tooltip 10px below cursor
       // if tooltip is close to the bottom of the window, put tooltip 10px above cursor
-      let tooltipHeight = parseInt(d3.select(this).style("height"));
+      var tooltipHeight = parseInt(d3.select(this).style("height"));
       if (event.clientY + tooltipHeight + 10 < window.innerHeight) {
         return "" + (event.clientY + 10) + "px";
       } else {
@@ -63,7 +65,7 @@ module vgTooltip {
     .style("left", function() {
       // by default: put tooltip 10px to the right of cursor
       // if tooltip is close to the right edge of the window, put tooltip 10 px to the left of cursor
-      let tooltipWidth = parseInt(d3.select(this).style("width"));
+      var tooltipWidth = parseInt(d3.select(this).style("width"));
       if (event.clientX + tooltipWidth + 10 < window.innerWidth) {
         return "" + (event.clientX + 10) + "px";
       } else {
@@ -73,8 +75,7 @@ module vgTooltip {
   }
 
   function clear() {
-    let tooltipRows = d3.select("#vis-tooltip").selectAll(".tooltip-row").data([]);
+    var tooltipRows = d3.select("#vis-tooltip").selectAll(".tooltip-row").data([]);
     tooltipRows.exit().remove();
     d3.select("#vis-tooltip").style("opacity", 0);
   }
-}
