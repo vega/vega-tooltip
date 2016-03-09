ea685b13a77a8bf392c91d4f5bae2d6e7f70d308
ZeningQu <quzening@gmail.com>
2016-03-09 01:55:55 -0800

    Allow specifying time format in options

--- src/vg-tooltip.js
+++ src/vg-tooltip.js
@@ -1,6 +1,20 @@
 var tooltipUtil = function() {
 
-  function update_position(event) {
+  // decide if a chart element deserves tooltip
+  function shouldShowTooltip (item) {
+    if (!item || !item.datum) return false;
+
+    // avoid showing tooltip for facet's background
+    if (item.datum._facetID) return false;
+
+    // avoid showing tooltip for axis title and labels
+    if (!item.datum._id) return false;
+
+    return true;
+  }
+
+  // update tooltip position
+  function updatePosition(event) {
     d3.select("#vis-tooltip")
     .style("top", function() {
       // by default: put tooltip 10px below cursor
@@ -26,13 +40,7 @@ var tooltipUtil = function() {
 
   return {
     fill: function (event, item, options) {
-      if (!item || !item.datum) return;
-
-      // avoid showing tooltip for facet's background
-      if (item.datum._facetID) return;
-
-      // avoid showing tooltip for axis title and labels
-      if (!item.datum._id) return;
+      if(shouldShowTooltip(item) === false) return;
 
       var itemData = d3.map(item.datum);
 
@@ -40,29 +48,56 @@ var tooltipUtil = function() {
       // if options is empty, auto determine fields to show
       if (options && options.length > 0) {
         var opt = d3.map(options, function(d) { return d.field; });
-        itemData.forEach(function(key, value) {
-          if(opt.has(key)) {
+        itemData.forEach(function(fieldName, fieldValue) {
+          if(opt.has(fieldName)) {
             // keep the field in tooltip
             // format the field for tooltip
+
+            switch(opt.get(fieldName).type) {
+              case "utc":
+              case "date":
+              // format date as specified in opt
+              // otherwise, auto format date
+              if(opt.get(fieldName).format) {
+                var custTimeFmt = dl.format.time(opt.get(fieldName).format);
+                itemData.set(fieldName, custTimeFmt(fieldValue));
+              }
+              else {
+                var autoTimeFmt = dl.format.auto.time();
+                itemData.set(fieldName, autoTimeFmt(fieldValue));
+              }
+              break;
+              case "number":
+              // format number
+              break;
+              case "string":
+              // format string
+              break;
+              case "object":
+              // format object
+              break;
+              default:
+              // infer field type and auto format it
+            }
           }
           else {
             // remove field from tooltip
-            itemData.remove(key);
+            itemData.remove(fieldName);
           }
         })
       }
       else {
-        console.log("empty: " + options);
         // remove vega internals
         itemData.remove("_id");
         itemData.remove("_prev");
-      }
 
+        // infer field types and auto-format them
+
+      }
 
       // get array of key value pairs for tooltip
       var tooltipData = itemData.entries();
 
-      // TODO(zening): show partial data in tooltip
       var tooltipRows = d3.select("#vis-tooltip").selectAll(".tooltip-row").data(tooltipData);
 
       tooltipRows.exit().remove();
@@ -70,19 +105,13 @@ var tooltipUtil = function() {
       var row = tooltipRows.enter().append("tr")
       .attr("class", "tooltip-row");
       row.append("td").attr("class", "key").text(function(d) { return d.key + ":"; });
-      row.append("td").attr("class", "value").text(function(d) {
-        var timeFormatter = dl.format.auto.time();
-        switch(dl.type(d.value)) {
-          case "date": return timeFormatter(d.value);
-          default: return d.value;
-        }
-      });
+      row.append("td").attr("class", "value").text(function(d) { return d.value; });
 
-      update_position(event);
+      updatePosition(event);
       d3.select("#vis-tooltip").style("opacity", 1);
     },
     update: function(event, item) {
-      update_position(event);
+      updatePosition(event);
     },
     clear: function() {
       var tooltipRows = d3.select("#vis-tooltip").selectAll(".tooltip-row").data([]);
@@ -152,4 +181,4 @@ var vlTooltip = function() {
       view.on("mouseout", tooltipUtil.clear);
     }
   }
-}()
+}();
