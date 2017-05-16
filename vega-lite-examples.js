ec7dff3d72cf0a0ac083e0c9d303c9c172ba95cf
sirahd <sirah@uw.edu>
2017-05-16 14:09:00 -0700

    add vega and vega-lite tooltip examples separately

--- vega-lite-examples.js
+++ vega-lite-examples.js
@@ -19,20 +19,6 @@
     });
   }
 
-  function addVgExample(path, id, options) {
-    d3.json(path, function(error, vgSpec) {
-      if (error) {
-        return console.error(error);
-      }
-      vega.embed(id, vgSpec, undefined, function(error, result) {
-        if (error) {
-          return console.error(error);
-        }
-        vegaTooltip.vega(result.view, options);
-      });
-    })
-  }
-
   /* Vega-Lite Examples */
   // Scatter Plot
   var scatterOpts = {
@@ -124,54 +110,4 @@
     ]
   }
   addVlExample("exampleSpecs/overlay_area_short.json", "#vis-overlay-area", overlayAreaOpts);
-  
-  // Vega Examples 
-  // Arc
-  addVgExample("exampleSpecs/arc.json", "#vis-arc");
-
-  // Choropleth
-  var choroplethOpts = {
-    showAllFields: false,
-    fields: [
-      {
-        field: "unemp.id",
-        title: "County ID",
-        formatType: "string"
-      },
-      {
-        field: "unemp.rate",
-        title: "Unemployment Rate",
-        formatType: "number",
-        format: ".1%"
-      }
-    ]
-  }
-  addVgExample("exampleSpecs/choropleth.json", "#vis-choropleth", choroplethOpts);
-
-  // Force
-  var forceOpts = {
-    showAllFields: false,
-    fields: [
-      { field: "name" }
-    ]
-  }
-  addVgExample("exampleSpecs/force.json", "#vis-force", forceOpts);
-
-  // Heatmap
-  var heatmapOpts = {
-    showAllFields: false,
-    fields: [
-      {
-        field: "temp",
-        title: "temp(F)"
-      },
-      {
-        field: "date",
-        formatType: "time",
-        format: "%B %e"
-      },
-      { field: "hour" }
-    ]
-  }
-  addVgExample("exampleSpecs/heatmap.json", "#vis-heatmap", heatmapOpts);
 }());
