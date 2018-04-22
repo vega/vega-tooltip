2f6c62a4d8ac2b09bc530ef686d2619a6e907ec5
Dominik Moritz <domoritz@gmail.com>
2018-04-21 20:34:47 -0700

    Deploy gh pages

---
+++ scripts/deploy-gh.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+
+npm run clean
+
+git checkout gh-pages
+git pull
+git merge master --no-edit
+
+# build
+npm run build
+git add build/* -f
+
+# commit if things changed
+if [ -n "$(git status --porcelain)" ]; then
+  git commit -m "Deploy Github Pages"
+fi
+
+# push and return to master!
+git push
+git checkout master
