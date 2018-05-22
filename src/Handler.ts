981ebd02d8a23c487df936caed01e6c1a0ba9cae
silky <noonsilk+-noonsilk@gmail.com>
2018-05-22 15:49:30 +1000

    fixes #205

--- src/Handler.ts
+++ src/Handler.ts
@@ -72,7 +72,7 @@ export class Handler {
     }
 
     // set the tooltip content
-    this.el.innerHTML = formatValue(value, escapeHTML, this.options.maxDepth);
+    this.el.innerHTML = formatValue(value, this.options.sanitize, this.options.maxDepth);
 
     // make the tooltip visible
     this.el.classList.add('visible', `${this.options.theme}-theme`);
