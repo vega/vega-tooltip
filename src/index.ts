5083fc4ed9c6d4a6bd284ceca256b8562f4a5ac4
siddhant <30566406+siddhant1@users.noreply.github.com>
2019-07-25 17:32:25 +0530

    Add version to module (#256)

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
+import pkg from '../package.json';
 import {View} from 'vega-typings';
 
 import {Options} from './defaults';
 import {Handler} from './Handler';
 
+const version = pkg.version;
+
 export * from './defaults';
 export * from './formatValue';
 export * from './position';
 export * from './Handler';
+export {version};
 
 /**
  * Create a tooltip handler and register it with the provided view.
