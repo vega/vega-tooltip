3492570f445cb6516e0a58e81e0ae57afec645af
Dominik Moritz <domoritz@gmail.com>
2020-09-10 07:04:33 -0700

    fix: switch to umd
    fixes #472

--- rollup.config.js
+++ rollup.config.js
@@ -43,7 +43,7 @@ const outputs = [
       },
       {
         file: pkg.unpkg,
-        format: 'iife',
+        format: 'umd',
         name: 'vegaTooltip',
         exports: 'named',
         globals: {
