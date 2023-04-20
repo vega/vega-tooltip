e1703fdad9fee74ac1ddbdb4616cbb1948b4b0ed
Dominik Moritz <domoritz@gmail.com>
2023-04-20 21:57:31 +0200

    feat: remove beemo

--- .config/beemo/typescript.ts
+++
@@ -1,9 +0,0 @@
-import {TypeScriptConfig} from '@beemo/driver-typescript';
-
-const config: TypeScriptConfig = {
-  compilerOptions: {
-    typeRoots: ['node_modules/@types', 'types'],
-  },
-};
-
-export default config;
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,8 @@
 import json from '@rollup/plugin-json';
 import resolve from '@rollup/plugin-node-resolve';
-import ts from 'rollup-plugin-ts';
-import bundleSize from 'rollup-plugin-bundle-size';
 import terser from '@rollup/plugin-terser';
+import bundleSize from 'rollup-plugin-bundle-size';
+import ts from 'rollup-plugin-ts';
 
 import pkg from './package.json' assert { type: 'json' };
 
@@ -55,7 +55,7 @@ const outputs = [
         plugins: [terser()],
       }
     ],
-    plugins: plugins('> 1%, not dead', false),
+    plugins: plugins('defaults', false),
     external: ['vega-util']
   }
 ];
